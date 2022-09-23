287d556feb60dd28893cb09e1cf2f26aa518e6b2
Satish1550 <satishsunny1550@gmail.com>
2022-09-23 23:17:05 +0530

    format

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
 let count = 0;
 function increment() {
-    count+=1;
-    countEl.innerHTML=count;
+    count += 1;
+    countEl.innerHTML = count;
 }
-function save(){
-    let countStr=count+ " - ";
-    saveEl.innerHTML +=countStr;
-    countEl.innerHTML=0;
-    count=0;
+function save() {
+    let countStr = count + " - ";
+    saveEl.innerHTML += countStr;
+    countEl.innerHTML = 0;
+    count = 0;
 }
-function reset(){
-   countEl.innerHTML=0;
-   saveEl.textContent="details :";
+function reset() {
+    countEl.innerHTML = 0;
+    saveEl.textContent = "details :";
 
 }
